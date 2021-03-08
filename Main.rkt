16355c481712144e87b547805f139b301821c776
error4days <justin.j.lee14@gmail.com>
2021-03-08 15:08:19 -0500

    Cleaned up code and added abstraction. Comments were added.

--- Main.rkt
+++ Main.rkt
@@ -7,7 +7,8 @@
 (define operator car)
 (define leftoperand cadr)
 (define rightoperand caddr)
-(define getExpression cadar)
+(define getReturnExpression cadar)
+(define operands cdr)
 (define member*?
   (lambda (a lis)
     (cond
@@ -15,14 +16,7 @@
       [(pair? (car lis)) (or (member*? a (car lis)) (member*? a (cdr lis)))]
       [(eq? a (car lis)) #t]
       [else (member*? a (cdr lis))])))
-; Finds a subarray and replaces it (replace '(z) '((a 4) (d 3223) (z 3)))
-(define replace
-  (lambda (og new lis)
-    (cond
-      [(null? lis) (cons new '())]
-      [(member*? og (car lis)) (cons new (cdr lis))]
-      [else (cons (car lis) (replace og new (cdr lis)))])))
-; Same as replace but throws an error if the variable can't be found
+; Inserts a varaible or a variable number pair
 (define insert
   (lambda (og new lis)
     (cond
@@ -36,14 +30,17 @@
       [(null? lis) (error 'variable-not-declared)]
       [(member*? varName (car lis)) (cadr (car lis))]
       [else (getValue varName (cdr lis))])))
-; Gets value from variable or value
+; Takes in variable or value and returns a value
 (define int_val
   (lambda (val vars)
     (cond
       [(number? val) val]
       [(member*? val vars)(getValue val vars)]
       [else val])))
-
+; Takes two atoms and returns them in a list
+(define listMaker
+  (lambda (a b)
+    (cons a (cons b '()))))
 
 
 ; M-integer maps expressions to integer values
@@ -64,7 +61,7 @@
                                                 (M-integer (int_val(rightoperand expression)vars)vars))]
       [(eq? (operator expression) '%) (modulo (M-integer (int_val(leftoperand expression)vars)vars)
                                               (M-integer (int_val(rightoperand expression)vars)vars))]
-      [else (error 'bad-operator)])))
+      [else (error 'bad-math-operator)])))
 
 ; M-compare maps comparisons to boolean values
 ; operators: ==, !=, <, >, <=. >=
@@ -84,96 +81,101 @@
                                            (M-evaluate (int_val(rightoperand expression)vars) vars))]
       [(eq? (operator expression) '!=) (not (eq? (M-evaluate (int_val(leftoperand expression)vars) vars)
                                            (M-evaluate (int_val(rightoperand expression)vars) vars)))]
-      [else (error 'bad-operator)])))
+      [else (error 'bad-compare-operator)])))
 
-; M-bool maps boolean expressions to boolean values and converts strings to booleans, True -> #t
+; M-bool maps boolean expressions to boolean values and converts strings to booleans
 ; operators: &&, ||, !
 (define M-bool
   (lambda (expression vars)
     (cond
-      [(boolean? expression) expression]
-      [(eq? expression 'true) #t]
-      [(eq? expression 'false) #f]
-      [(and (not (list? expression)) (boolean? (getValue expression vars))) (getValue expression vars)]
-      [(and (eq? (operator expression) '!) (boolean? (M-evaluate (leftoperand expression) vars))) (not (M-bool (leftoperand expression) vars))]
-      [(not (boolean? (M-evaluate (leftoperand expression) vars))) (error 'input-not-boolean)]
-      [(not (boolean? (M-evaluate (rightoperand expression) vars))) (error 'input-not-boolean)]
+      [(boolean? expression) expression] ; if already boolean, return boolean
+      [(eq? expression 'true) #t] ; if true, return #t
+      [(eq? expression 'false) #f] ; if false, return #f
+      [(and (not (list? expression)) (boolean? (getValue expression vars))) (getValue expression vars)] ; if variable and variable is boolean, return boolean
+      [(and (eq? (operator expression) '!) (boolean? (M-evaluate (leftoperand expression) vars)))
+       (not (M-bool (leftoperand expression) vars))]  ; if ! statement and boolean value, return opposite
+      [(not (boolean? (M-evaluate (leftoperand expression) vars))) (error 'input-not-boolean)] ; if leftoperand not boolean, error
+      [(not (boolean? (M-evaluate (rightoperand expression) vars))) (error 'input-not-boolean)] ; if rightoperand not boolean, error
       [(eq? (operator expression) '&&) (and (M-evaluate (leftoperand expression) vars)
-                                            (M-evaluate (rightoperand expression) vars))]
+                                            (M-evaluate (rightoperand expression) vars))] ; if &&, return (and leftoperand rightoperand)
       [(eq? (operator expression) '||) (or (M-evaluate (leftoperand expression) vars)
-                                           (M-evaluate (rightoperand expression) vars))]
-      [else (error 'bad-operator)])))
+                                           (M-evaluate (rightoperand expression) vars))] ; if ||, return (or leftoperand rightoperand)
+      [else (error 'bad-boolean-operator)])))
 
 ; Variable declaration
 ; '(var x) or '(var x (expression))
 (define M-declare
   (lambda (expression vars)
     (cond
-      [(not (null? (cddr expression))) (M-assign (cons '= (cons (cadr expression) (cons (M-evaluate (caddr expression) vars)'())))
-                                                                (M-declare (cons 'var (cons (cadr expression) '())) vars))] ;Abstract
-      [(member*? (leftoperand expression) vars) (error 'redefining-variable)]
+      [(not (null? (cddr expression))) (M-assign (cons '= (listMaker (leftoperand expression) (M-evaluate (rightoperand expression) vars)))
+                                                                (M-declare (listMaker 'var (leftoperand expression)) vars))] ; Declaration with an assignment
+      [(member*? (leftoperand expression) vars) (error 'redefining-variable)] ; 
       [else (cons (cons (leftoperand expression) '()) vars)])))
 
 ; Maps variables with values
 ; '(= x value/expression)
 (define M-assign
   (lambda (expression vars)
-    (insert (cadr expression) (M-evaluate (car (cddr expression)) vars) vars)))
+    (insert (leftoperand expression) (M-evaluate (rightoperand expression) vars) vars)))
 
 ; if statements
+; '(if (> x y) (return x)
 (define M-if
   (lambda (expression next vars)
     (cond
-      ;[(not (boolean? (M-evaluate (car (cdr expression)) vars))) (error 'input-not-boolean)]
-      [(and (eq? (car expression) 'if) (M-evaluate (cadr expression) vars)) (M-if (caddr expression) next vars)] ; if statement true
-      [(and (eq? (car expression) 'if) (null? (cdddr expression))) (M-state next vars)]
-      [(eq? (car expression) 'if) (M-if (cadddr expression) next vars)] ;run else/if
-      [(eq? (operator expression) 'return) (M-return (cadr expression) vars)] ; run return function
-      [(eq? (operator expression) '=) (M-state next (M-assign expression vars))] ;to assign values
+      [(and (eq? (operator expression) 'if) (M-evaluate (leftoperand expression) vars)) (M-if (rightoperand expression) next vars)] ; if condition is true, run body
+      [(and (eq? (operator expression) 'if) (null? (cdddr expression))) (M-state next vars)] ; No else statement
+      [(eq? (operator expression) 'if) (M-if (cadddr expression) next vars)] ;Run else/ else if
+      [(eq? (operator expression) 'return) (M-return (cadr expression) vars)] ; Finds and runs return function
+      [(eq? (operator expression) '=) (M-state next (M-assign expression vars))] ; Executes variable assignment values
       [else (error 'invalid-if-statement)])))
 
 ; while statements
+; '(while (!= (% y x) 3) (= y (+ y 1)))
 (define M-while
   (lambda (expression next vars)
     (cond
-      [(and (eq? (car expression) 'while) (M-evaluate (cadr expression) vars))
-       (M-while expression next (M-while (caddr expression) next vars))] ; Enter loop, run body
-      [(eq? (car expression) 'while) (M-state next vars)] ; Exit loop
-      [(eq? (operator expression) '=) (M-assign expression vars)] ; Run body with assignment
-      [else (error 'invalid-while-loop)]))) ; Error
+      [(and (eq? (operator expression) 'while) (M-evaluate (leftoperand expression) vars))
+       (M-while expression next (M-while (rightoperand expression) next vars))] ; Enter loop, run body
+      [(eq? (operator expression) 'while) (M-state next vars)] ; Exit loop
+      [(eq? (operator expression) '=) (M-assign expression vars)] ; Body has assignment, runs assign
+      [else (error 'invalid-while-loop)])))
       
 
 ; Passes the expression into the correct function for evaluation
 (define M-evaluate
   (lambda (expression vars)
     (cond
-      [(or (eq? expression 'false) (eq? expression 'true)) (M-bool expression vars)]
-      [(not (list? expression)) (int_val expression vars)]
-      [(member*? (operator expression) '(+ - * / %)) (M-integer expression vars)]
-      [(member*? (operator expression) '(== != < > <= >=)) (M-compare expression vars)]
-      [(member*? (operator expression) '( && || !)) (M-bool expression vars)]
+      [(or (eq? expression 'false) (eq? expression 'true)) (M-bool expression vars)] ; Given a single boolean
+      [(not (list? expression)) (int_val expression vars)] ; Given an atom that is a variable or value
+      [(member*? (operator expression) '(+ - * / %)) (M-integer expression vars)] ; Math expression
+      [(member*? (operator expression) '(== != < > <= >=)) (M-compare expression vars)] ; Comparison expression
+      [(member*? (operator expression) '( && || !)) (M-bool expression vars)] ; Boolean expression
       [else (error 'no-valid-operator)])))
 
 ; Returns a value or boolean
+; '(M-return 'x)
 (define M-return
   (lambda (expression vars)
     (cond
-      [(list? expression) (M-evaluate expression vars)]
-      [(number? expression) expression]
-      [(eq? expression #t) 'true]
-      [(eq? expression #f) 'false]
-      [else (getValue expression vars)])))
+      [(list? expression) (M-evaluate expression vars)] ; Expression hasn't been evaluated
+      [(number? expression) expression] ; Given a number
+      [(eq? expression #t) 'true] ; Given a boolean
+      [(eq? expression #f) 'false] ; Given a boolean
+      [else (getValue expression vars)]))) ; Given a variable
 
+; Variables stored as '((x 3) (y) (i 7)) in vars
 (define M-state
   (lambda (expression vars)
     (cond
       [(null? expression) 'void]
-      [(eq? (operator (car expression)) 'return) (M-return (getExpression expression) vars)]
+      [(eq? (operator (car expression)) 'return) (M-return (getReturnExpression expression) vars)]
       [(eq? (operator (car expression)) 'var) (M-state (cdr expression) (M-declare (car expression) vars))]
       [(eq? (operator (car expression)) '=) (M-state (cdr expression) (M-assign (car expression) vars))]
       [(eq? (operator (car expression)) 'if) (M-if (car expression) (cdr expression) vars)]
       [(eq? (operator (car expression)) 'while) (M-while (car expression) (cdr expression) vars)])))
 
+; Initializes M-state with an empty variable list
 (define M-state-start
   (lambda (expression)
     (M-state expression '())))

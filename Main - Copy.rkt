8a55aa0f2037770818f835e695987db589831876
lizshu <32874551+lizshu@users.noreply.github.com>
2021-03-28 23:57:16 -0500

    Fixed code for test #13

--- Main - Copy.rkt
+++ Main - Copy.rkt
@@ -272,6 +272,7 @@
 (define M-state
   (lambda (expression outer vars returns)
     (cond
+      [(and (null? expression) (null? vars) (null? outer)) (error 'break-not-in-loop)]
       [(null? expression) vars]
       [(eq? (operator (nextExecute expression)) 'return)
        (M-return (getExpression expression) vars returns)]
@@ -347,8 +348,8 @@
 (interpret "Tests/Test38")     ;output should be 100 |#
 
 ;;; TESTS FOR INTERPRETER PT2
-#|
-(interpret "Tests2/Test1")    ;20
+
+#|(interpret "Tests2/Test1")    ;20
 (interpret "Tests2/Test2")    ;164
 (interpret "Tests2/Test3")    ;32
 (interpret "Tests2/Test4")    ;2
@@ -357,11 +358,11 @@
 (interpret "Tests2/Test7")    ;21
 (interpret "Tests2/Test8")    ;6
 (interpret "Tests2/Test9")    ;-1 |#
-(interpret "Tests2/Test10")   ;789
-;(interpret "Tests2/Test11")   ;Error
-;(interpret "Tests2/Test12")   ;Error
-;(interpret "Tests2/Test13")   ;Error
-;(interpret "Tests2/Test14")   ;12
+;(interpret "Tests2/Test10")  ;789
+;(interpret "Tests2/Test11")  ;Error
+;(interpret "Tests2/Test12")  ;Error
+(interpret "Tests2/Test13")   ;Error
+(interpret "Tests2/Test14")   ;12
 #|(interpret "Tests2/Test15")   ;125
 (interpret "Tests2/Test16")   ;110
 (interpret "Tests2/Test17")   ;2000400
